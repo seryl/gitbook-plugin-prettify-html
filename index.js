1d1f4e1a1d3c86a8cb2d3fa3f3beedd2447aee5a
Josh Toft <josh@pack.io>
2016-06-27 23:06:07 -0700

    Correctly handle in-place replacements

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+var path = require('path');
+var fs = require('fs');
 var html = require('html');
+
+var updatedPages = [];
+
+function getTargetHtmlPath(mdPath) {
+  var mdInfo = path.parse(mdPath);
+  var targetFile = mdInfo.base.replace(".md", ".html");
+
+  if (mdInfo.base == "README.md") {
+    targetFile = "index.html";
+  }
+
+  var relpath = path.join("./", "_book", mdInfo.dir, targetFile);
+  return path.resolve(relpath);
+}
+
+function prettify(str) {
+  return html.prettyPrint(str);
+}
+
+function prettifyFile(filename) {
+  fs.writeFileSync(filename, prettify(fs.readFileSync(filename).toString()));
+}
 
 module.exports = {
   book: {},
   hooks: {
     "page": function (page) {
-      page.content = html.prettyPrint(page.content);
+      updatedPages.push(page.path);
       return page;
+    },
+    "finish": function() {
+      console.log("Current Directory");
+      console.log(process.cwd());
+
+      for (var i=0; i < updatedPages.length; i++) {
+        var hPath = getTargetHtmlPath(updatedPages[i]);
+        prettifyFile(hPath);
+      }
+
+      updatedPages = [];
     }
   }
 };
